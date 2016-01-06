d3df1ce68a41a94bccd0caa2eb888c47a5b130a8
gstruwig <graham@datamatix.co.za>
2016-01-06 12:32:34 +0200

    tst

--- app/src/main/java/za/co/datamatix/dontsayit/MainActivity.java
+++ app/src/main/java/za/co/datamatix/dontsayit/MainActivity.java
@@ -121,7 +121,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private void hasValueDoRest(String val) {
         maxFieldNo = new Long(val).longValue();
     }
-
+//test
 
     @Override
     public void onClick(View v) {
